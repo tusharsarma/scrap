aa6213c8552281259c9b0cd66fd7461b6c0fed8c
tusharsarma <tusharsarma20@gmail.com>
2019-04-28 17:13:22 +0530

    modification

--- index.js
+++ index.js
@@ -4,62 +4,77 @@ const Table = require('cli-table');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
+const axios = require('axios');
+const readline = require('readline');
 
 
-function scraping(options, filename) {
+function scraping(options, filename, website) {
   const output = [];
   var oneFlightData = [];
-  var tableHeader = [];
-  var header;
   var flightInformation;
 
-  rp(options)
-    .then( $ => {
-      
-      $('div.x21n .x7b tr th.x7w').each((i, tableRow) => {
-        header = $(tableRow).text();
-        tableHeader.push(header);
-      });
-      output.push(tableHeader.join());
 
-      let table = new Table({
-        head: tableHeader,
-        colWidths: [15, 15, 15, 10, 10, 15]
-      })
+  axios.get(options)
+    .then((response) => {
+      if (response.status === 200) {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const scrapDetails = website.toString()=="DEL" ? $('.flights_scroll_nou #flight_detail') : $('div.x21n .x7b tbody tr');
+        scrapDetails.each((i, flightRow) => {
+          oneFlightData = [];
+          const inner = website.toString()=="DEL" ? 'div#flight_detail_junt div, div#flight_detail_junt2 div' : 'td,th';
+          $(flightRow).find(inner).each((l, data) => {
+            flightInformation = $(data).text().split('(')[0];
 
-      $('div.x21n .x7b tr').each((i, flightRow) => {
-        oneFlightData = [];
-        $(flightRow).find('.x7m').each((l, data) => {
-          flightInformation = $(data).text();
-
-          oneFlightData.push(flightInformation);
+            oneFlightData.push(flightInformation);
+          });
+          if (oneFlightData.length > 0) {
+            output.push(oneFlightData.join(','));
+          }
         });
-        if (oneFlightData.length > 0) {
-          table.push(oneFlightData);
-          output.push(oneFlightData.join());
-        }
-      });
-      console.log(table.toString());
 
-      fs.writeFileSync(filename, output.join(os.EOL));
-      console.log(filename, "  CREATED");
-    }).catch(err =>
-      console.log(err))
+
+        fs.writeFileSync(filename, output.join(os.EOL));
+        console.log(filename, "  CREATED");
+      }
+    }).catch(error => console.log(error));
 
 }
 
 
-var departure = {
-  url: `http://www.bengaluruairport.com/bial/faces/pages/flightInformation/departures.jspx`,
-  transform: body => cheerio.load(body)
-}
+const departure = `http://www.bengaluruairport.com/bial/faces/pages/flightInformation/departures.jspx`;
 
-var arrival = {
-  url: `http://www.bengaluruairport.com/flightInformation/arrivals.jspx`,
-  transform: body => cheerio.load(body)
-}
+const arrival = `http://www.bengaluruairport.com/flightInformation/arrivals.jspx`;
+
+const delhiURL = `https://www.delhiairport.com/igi-indira-gandhi-arrivals`;
 
 const fileArrival = path.join(__dirname, 'arrival.csv');
 const fileDeparture = path.join(__dirname, 'departure.csv');
-scraping(departure, fileDeparture);
-scraping(arrival, fileArrival);
+const delhiData = path.join(__dirname, 'delhi.csv');
+
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
+rl.question('Please enter "BA" for BEN arrival data, "BD" for BEN Departure and "D" for Delhi data: ', (answer) => {
+
+  if (answer.toUpperCase() == "BA") {
+    scraping(arrival, fileArrival, "BEN");
+    rl.close();
+  } else if (answer.toUpperCase() == "BD") {
+    scraping(departure, fileDeparture, "BEN");
+    rl.close();
+  } else if (answer.toUpperCase() == "D") {
+    scraping(delhiURL, delhiData, "DEL");
+    rl.close();
+  } else {
+    console.log("your input is not valid");
+    rl.close();
+  }
+
+});
